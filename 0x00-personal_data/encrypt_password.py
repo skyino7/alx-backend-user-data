67b7d828320e7c7a41f4913b7c3002e902e7ec3e
Seth Ketsi <christambassador12@gmail.com>
2024-05-16 22:58:38 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,12 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password
+    matches the hashed password.
+    """
+    check = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return check
