bb00e38ec8fdecafdfe2f1be42d4efe56972bc7b
Seth Ketsi <christambassador12@gmail.com>
2024-05-16 22:34:46 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -71,7 +71,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         password=password,
         host=host,
         database=db_name,
-        auth_plugin='mysql_native_password'
     )
 
 
