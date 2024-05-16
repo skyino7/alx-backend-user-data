4b6ddcc92bcb705a5fcc4d1470f3835ad7a3a87c
Seth Ketsi <christambassador12@gmail.com>
2024-05-16 14:22:54 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Returns the log message obfuscated
     """
