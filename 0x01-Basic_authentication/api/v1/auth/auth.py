f6a516835b77072d3dc8f91aed0239bb5b1e2ccb
Seth Ketsi <christambassador12@gmail.com>
2024-05-20 18:50:42 +0000

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,17 +4,22 @@
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth:
     """Auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth method"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Authorization header method"""
-        return False
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method"""
