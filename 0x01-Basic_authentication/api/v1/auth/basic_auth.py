b7f199ba34d7aff6b866f586801e301cbf558afc
Seth Ketsi <christambassador12@gmail.com>
2024-05-20 19:41:44 +0000

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,14 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extract base64 authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
