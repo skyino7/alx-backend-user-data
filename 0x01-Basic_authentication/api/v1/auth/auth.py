d9b5b71b6236d96747f597bc02d04f23c147bb2b
Seth Ketsi <christambassador12@gmail.com>
2024-05-20 23:52:55 +0000

    updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,17 +10,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth method"""
-        if path is None or not excluded_paths:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
+        if path[-1] != '/':
+            path += '/'
         path = path.rstrip('/')
 
         for excluded_path in excluded_paths:
             if excluded_path.endswith('*'):
                 if path.startswith(excluded_path.rstrip('*')):
                     return False
-                elif path == excluded_path.rstrip('/'):
-                    return False
+            elif path == excluded_path.rstrip('/'):
+                return False
 
         return True
 
