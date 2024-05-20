87767881895448dfa6e4bcce64fc1df16c90e81c
Seth Ketsi <christambassador12@gmail.com>
2024-05-20 20:34:10 +0000

    10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Create a class BasicAuth that inherits from Auth
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -54,3 +55,21 @@ class BasicAuth(Auth):
             return None, None
         user_email, user_pwd = decoded.split(":", 1)
         return user_email, user_pwd
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> 'User':
+        """User Object From Credentials"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not users:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
