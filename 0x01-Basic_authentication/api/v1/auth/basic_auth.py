0c60d7c3f5c9db77383310b2a62a7102fc403069
Seth Ketsi <christambassador12@gmail.com>
2024-05-20 19:03:51 +0000

    6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicAuth that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+    pass
