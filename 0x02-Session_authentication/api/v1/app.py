79572303a8b5bdfb3d602748102dffbfcbfd5b16
Seth Ketsi <christambassador12@gmail.com>
2024-05-22 22:40:51 +0000

    updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,6 +59,8 @@ def before_request() -> str:
             abort(401)
         if not auth.current_user(request):
             abort(403)
+    if auth:
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
