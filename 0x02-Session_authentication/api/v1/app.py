c32112c72b3c3a1afe5c73891ba4b3abff404d28
Seth Ketsi <christambassador12@gmail.com>
2024-05-23 10:40:15 +0000

    Fixed

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,11 +47,19 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ Before request handler
+    """
+    Before request handler
     """
     if auth is None:
         return
-
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
     if auth:
         request.current_user = auth.current_user(request)
 
