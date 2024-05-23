2e6ace215b831438cc8d65dc2fba5be316d4841b
Seth Ketsi <christambassador12@gmail.com>
2024-05-23 11:28:40 +0000

    updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,9 @@ def before_request() -> str:
                           '/api/v1/forbidden/',
                           '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
