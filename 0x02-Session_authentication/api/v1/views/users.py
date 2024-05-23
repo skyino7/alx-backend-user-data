ebe16ec119465ca381a27cb7a32063a94739da66
Seth Ketsi <christambassador12@gmail.com>
2024-05-23 10:49:25 +0000

    updated

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -120,19 +120,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
-
-@app_views.route('/api/v1/users/<user_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_user(user_id):
-    """ Retrieves a user """
-    if user_id == 'me':
-        if request.current_user is None:
-            abort(404)
-        else:
-            user = request.current_user
-            return jsonify(user.to_json())
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    return jsonify(user.to_json())
