74bd8134744bbddf366046c2389445febf8d366c
Seth Ketsi <christambassador12@gmail.com>
2024-05-28 14:54:49 +0000

    Check

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm.session import Session
 from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import TypeVar
 
 
 class DB:
