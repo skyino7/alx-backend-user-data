9d01bf135556cf0772ad0651b410026b7d78a725
Seth Ketsi <christambassador12@gmail.com>
2024-05-29 08:09:17 +0000

    16. Generate reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,5 +85,19 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """
+    Returns Reset Password Token
+    """
+
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
